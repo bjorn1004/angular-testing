a84f47f6cc6c0ea28c234fccc31b00e7d0ce174c
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 19:03:28 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeMainComponent } from './modules/home/components/home-main/home-main.component';
+import { HomeModule } from './modules/home/home.module';
 
 const routes: Routes = [
-	{
-		path: '',
-		pathMatch: 'full',
-		redirectTo: 'home',
-	},
-	{
-		path: 'home',
-		loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
-	},
 	{
 		path: 'threejs',
 		loadChildren: () => import('./modules/three-js/three-js.module').then((m) => m.ThreeJSModule),
@@ -19,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes), HomeModule],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
